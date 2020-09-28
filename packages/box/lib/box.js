2160f0ee653c21a877c0de8e694b9dfb42b4c5e7
isamrish <askmaurya48@gmail.com>
2020-09-29 00:54:25 +0530

    Initial commit
    Signed-off-by: isamrish <askmaurya48@gmail.com>

---
+++ packages/box/lib/box.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { color } from "styled-system"
+
+function box() {
+    return (
+        <div>Box</div>
+    )
+}
---
+++ packages/button/lib/button.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { color } from "styled-system"
+
+function button() {
+    return (
+        <button>Button</button>
+    )
+}
